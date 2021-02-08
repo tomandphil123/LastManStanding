10d0aac8b2fea555560f9c4b92c7d2fb46722011
tcal97 <tom.callaghan4@mail.dcu.ie>
2021-02-08 22:00:08 +0000

    dealing with edge cases in lambdas

--- src/lastmanstanding/amplify/backend/function/joinLeague/src/index.py
+++ src/lastmanstanding/amplify/backend/function/joinLeague/src/index.py
@@ -7,18 +7,18 @@ def handler(event, context):
   print('received event:')
   print(event)
   result = json.loads(event['body'])
-  leagueCode = result["leagueCode"]
-  sub = result["sub"]
+  leagueCode = result['leagueCode']
+  sub = result['sub']
   fname = result['firstName']
   lname = result['lastName']
   fullName = fname + ' ' + lname
-  username = result["username"]
+  username = result['username']
 
   client = boto3.client('dynamodb')
 
   data = client.query(
-    TableName = "LeaguesDB-develop",
-    IndexName = "invitationCode-LeagueID-index",
+    TableName = 'LeaguesDB-develop',
+    IndexName = 'invitationCode-LeagueID-index',
     ExpressionAttributeValues={
         ':v1': {
             'S': leagueCode
@@ -31,25 +31,25 @@ def handler(event, context):
   leagueID = resp[0]['LeagueID']['S']
 
   leaguePlayerID = leagueID +'/'+ sub
-  admin = "No"
+  admin = 'No'
   createdDate = str(datetime.today())
   dynamodb = boto3.resource('dynamodb')
 
-  tableName = "LeaguePlayerDB-develop"
+  tableName = 'LeaguePlayerDB-develop'
   table = dynamodb.Table(tableName)
 
   table.put_item(
       Item={
 				'LeaguePlayerID': leaguePlayerID,
 				'LeagueID': leagueID,
-				'CurrentPick': " ",
+				'CurrentPick': '-',
 				'PickedTeams': [],
 				'Admin': admin,
-                'fullName': fullName,
+        'fullName': fullName,
 				'Username': username,
-				'playerStatus': "In",
+				'playerStatus': 'In',
 				'createdTime': createdDate,
-				'UnpickedTeams': ["Manchester United FC","Manchester City FC","Leicester City FC","Liverpool FC","Tottenham Hotspur FC","Everton FC","Chelsea FC","Southampton FC","West Ham United FC","Sheffield United FC","Arsenal FC","Aston Villa FC","Leeds United FC","Crystal Palace FC","Wolverhampton Wanderers FC","Newcastle United FC","Brighton & Hove Albion FC","Burnley FC","Fulham FC","West Bromwich Albion FC"]
+				'UnpickedTeams': ['Manchester United FC','Manchester City FC','Leicester City FC','Liverpool FC','Tottenham Hotspur FC','Everton FC','Chelsea FC','Southampton FC','West Ham United FC','Sheffield United FC','Arsenal FC','Aston Villa FC','Leeds United FC','Crystal Palace FC','Wolverhampton Wanderers FC','Newcastle United FC','Brighton & Hove Albion FC','Burnley FC','Fulham FC','West Bromwich Albion FC']
       })
 
   table3 = dynamodb.Table('PlayerDB-develop')
@@ -59,17 +59,18 @@ def handler(event, context):
 
   resp = data['Items']
   leagueIDs = resp[0]['leagueIDs']
-  leagueIDs.append(leagueID)
-  table3.update_item(
-    Key={
-            'Sub': sub
-        },
-        UpdateExpression="set leagueIDs=:l",
-        ExpressionAttributeValues={
-            ':l': leagueIDs
-        },
-        ReturnValues="UPDATED_NEW"
-  )
+  if leagueID not in leagueIDs:
+    leagueIDs.append(leagueID)
+    table3.update_item(
+      Key={
+              'Sub': sub
+          },
+          UpdateExpression='set leagueIDs=:l',
+          ExpressionAttributeValues={
+              ':l': leagueIDs
+          },
+          ReturnValues='UPDATED_NEW'
+    )
 
   return {
     'statusCode': 200,
@@ -78,6 +79,6 @@ def handler(event, context):
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
     },
-    'body': "hello"
+    'body': 'hello'
     }
 
