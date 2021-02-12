aa452966e3487aa3c22f50b58564931d9efd3227
pdonnelly <pdonnelly@shutterstock.com>
2021-02-12 16:02:09 +0000

    Split Crestbar component and added unit tests

---
+++ src/lastmanstanding/src/libs/crestBar/crests.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Crests = ({
+  screenWidth,
+  crests,
+}) => {
+  return (
+    typeof crests !== 'undefined' && screenWidth > 800 ? (
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      {crests.map((item) => (
+        <div style={{padding: '5px'}}
+          key={item['TeamName']}
+          className="img-wrapper">
+          <a href={item['Website']}>
+            <img src={item['Crest']}
+              alt= "team crests"
+              height="40px"
+              className="hover-zoom"/>
+          </a>
+        </div>
+      ))}
+    </div>
+    ) : (
+        null
+    )
+  );
+};
+
+Crests.propTypes = {
+  screenWidth: PropTypes.number.isRequired,
+  crests: PropTypes.array,
+};
+
+Crests.defaultProps = {
+  crests: [],
+};
+
+export default Crests;
---
+++ src/lastmanstanding/src/libs/crestBar/crests.spec.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Crests from './crests';
+
+describe('crestBar', () => {
+  const props = {
+    screenWidth: 1000,
+    crests: [
+      {
+        'Abbreviation': 'ARS',
+        'Crest': 'https://crests.football-data.org/57.svg',
+        'TeamName': 'Arsenal FC',
+        'Website': 'https://www.arsenal.com/',
+      },
+      {
+        'Abbreviation': 'AVL',
+        'Crest': 'https://crests.football-data.org/58.svg',
+        'TeamName': 'Aston Villa FC',
+        'Website': 'https://www.avfc.co.uk/',
+      },
+    ],
+  };
+  describe('Snapshot', () => {
+    it('renders', () => {
+      const component = shallow(
+          <Crests {...props}/>,
+      );
+      expect(component).toMatchSnapshot();
+    });
+  });
+});
