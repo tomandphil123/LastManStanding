09b1676c8bf31e860b22495e86c7b596d1547e0c
pdonnelly <pdonnelly@shutterstock.com>
2021-02-15 13:46:58 +0000

    Updated description and snapshot

--- src/lastmanstanding/src/libs/tables/premierLeagueResults.spec.jsx
+++ src/lastmanstanding/src/libs/tables/premierLeagueResults.spec.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import PremierLeagueResults from './premierLeagueResults';
 
-describe('loggedInHomePage', () => {
+describe('premierLeagueResults', () => {
   const props = {
     results: {
       data: [
