48d17c78171531673e8bf2411011c02e48d41b04
tcal97 <tom.callaghan4@mail.dcu.ie>
2021-03-08 23:54:34 +0000

    added automation to locking leagues

--- src/lastmanstanding/amplify/backend/function/LeagueInfoLambda/src/index.py
+++ src/lastmanstanding/amplify/backend/function/LeagueInfoLambda/src/index.py
@@ -19,7 +19,7 @@ def handler(event, context):
 		'Wolverhampton Wanderers FC': 'Wolves FC',
 		'Brighton & Hove Albion FC': 'Brighton FC'
 	}
-  
+
 	crests = {
 		'Manchester United FC':'https://crests.football-data.org/66.svg',
 		'Manchester City FC':'https://crests.football-data.org/65.svg',
@@ -32,138 +32,208 @@ def handler(event, context):
 		'West Ham United FC':'https://crests.football-data.org/563.svg',
 		'Sheffield United FC':'https://crests.football-data.org/356.svg',
 		'Arsenal FC':'https://crests.football-data.org/57.svg',
-        'Aston Villa FC':'https://crests.football-data.org/58.svg',
-        'Leeds United FC':'https://crests.football-data.org/341.svg',
-        'Crystal Palace FC':'https://crests.football-data.org/354.svg',
-        'Wolverhampton Wanderers FC':'https://crests.football-data.org/76.svg',
-        'Newcastle United FC':'https://crests.football-data.org/67.svg',
-        'Brighton & Hove Albion FC':'https://crests.football-data.org/397.svg',
-        'Burnley FC':'https://crests.football-data.org/328.svg',
-        'Fulham FC':'https://crests.football-data.org/63.svg',
-        'West Bromwich Albion FC': 'https://crests.football-data.org/74.svg',
+		'Aston Villa FC':'https://crests.football-data.org/58.svg',
+		'Leeds United FC':'https://crests.football-data.org/341.svg',
+		'Crystal Palace FC':'https://crests.football-data.org/354.svg',
+		'Wolverhampton Wanderers FC':'https://crests.football-data.org/76.svg',
+		'Newcastle United FC':'https://crests.football-data.org/67.svg',
+		'Brighton & Hove Albion FC':'https://crests.football-data.org/397.svg',
+		'Burnley FC':'https://crests.football-data.org/328.svg',
+		'Fulham FC':'https://crests.football-data.org/63.svg',
+		'West Bromwich Albion FC': 'https://crests.football-data.org/74.svg',
 	}
 
-	for team in response["standings"][0]["table"]:
-		if team["team"]["name"] in abbreviations:
-			TeamName = abbreviations[team["team"]["name"]]
-		else:
-			TeamName = team["team"]["name"]
-		# print(TeamName)
-		position = team["position"]
-		crestUrl = team["team"]["crestUrl"]
-		gamesPlayed = team["playedGames"]
-		won = team["won"]
-		draw = team["draw"]
-		lost = team["lost"]
-		points = team["points"]
-		goalDifference = team["goalDifference"]
-
-		dynamodb = boto3.resource('dynamodb')
-
-		tableName = "PlStandingsDB-develop"
-		table = dynamodb.Table(tableName)
-
-		table.put_item(
-			Item={
-				'TeamName': TeamName,
-				'position': str(position),
-				'crestUrl': crestUrl,
-				'gamesPlayed': str(gamesPlayed),
-				'won': str(won),
-				'draw': str(draw),
-				'lost': str(lost),
-				'points': str(points),
-				'goalDifference': str(goalDifference),
-				'createdTime': str(datetime.today())
-			}
-		)
-	
 	dynamodb = boto3.resource('dynamodb')
-	tableName1 = "PLFixturesDB-develop"
-	FixturesTable = dynamodb.Table(tableName1)
-	tableName2 = "PLResultsDB-develop"
-	ResultsTable = dynamodb.Table(tableName2)
-	scan2 = FixturesTable.scan()
-	with FixturesTable.batch_writer() as batch:
-		for each in scan2['Items']:
-			batch.delete_item(Key={'FixtureID':each['FixtureID']})
-			
-	scan1 = ResultsTable.scan()
-	with ResultsTable.batch_writer() as batch:
-		for each in scan1['Items']:
-			batch.delete_item(Key={'MatchID':each['MatchID']})
-			
-	for fixture in response2["matches"]:
-		if fixture["matchday"] > fixture["season"]["currentMatchday"] + 2:
-			break
+	schedulerDB = 'SchedulerDB-develop'
+	schedulerTable = dynamodb.Table(schedulerDB)
+	schedulerScan = schedulerTable.scan(AttributesToGet=['GameWeek'])
+	scheduler = schedulerScan['Items']
+	gamePeriod = scheduler[0]['GameWeek']
+	currWeek = response2['matches'][0]['season']['currentMatchday']
 
-		if fixture["matchday"] == fixture["season"]["currentMatchday"] - 1:
-			homeTeam = fixture["homeTeam"]["name"]
-			homeTeamCrest1 = crests[homeTeam]
-			awayTeam = fixture["awayTeam"]["name"]
-			awayTeamCrest1 = crests[awayTeam]
-			if homeTeam in abbreviations:
-				homeTeam = abbreviations[homeTeam]
-			if awayTeam in abbreviations:
-				awayTeam = abbreviations[awayTeam]
 
-			gameWeek = fixture["season"]["currentMatchday"]
-			homeTeamScore = fixture["score"]["fullTime"]["homeTeam"]
-			awayTeamScore = fixture["score"]["fullTime"]["awayTeam"]
-			if homeTeamScore == None and awayTeamScore == None:
-				homeTeamScore = "-"
-				awayTeamScore = "-"
-			if fixture["score"]["winner"] == "HOME_TEAM":
-				winner = fixture["homeTeam"]["name"]
-			elif fixture["score"]["winner"] == "AWAY_TEAM":
-				winner = fixture["awayTeam"]["name"]
+	if str(currWeek) != str(gamePeriod):
+		for fixture in response2['matches']:
+			if fixture['matchday'] == fixture['season']['currentMatchday']:
+				print(fixture['homeTeam']['name'], fixture['awayTeam']['name'])
+				firstGame = fixture['utcDate'].split('T')[1].split(':')
+				firstGameTime = firstGame[0] + ':' + firstGame[1]
+				firstGameDate = fixture['utcDate'].split('T')[0]
+				break
+
+
+
+		for team in response['standings'][0]['table']:
+			if team['team']['name'] in abbreviations:
+				TeamName = abbreviations[team['team']['name']]
 			else:
-				winner = "Draw"
+				TeamName = team['team']['name']
+			position = team['position']
+			crestUrl = team['team']['crestUrl']
+			gamesPlayed = team['playedGames']
+			won = team['won']
+			draw = team['draw']
+			lost = team['lost']
+			points = team['points']
+			goalDifference = team['goalDifference']
 
-			ResultsTable.put_item(
+			
+
+			tableName = 'PlStandingsDB-develop'
+			table = dynamodb.Table(tableName)
+
+			table.put_item(
 				Item={
-					'MatchID': homeTeam + "-" + awayTeam,
-					'HomeTeam': homeTeam,
-					'HomeTeamCrest': homeTeamCrest1,
-					'AwayTeam': awayTeam,
-					'AwayTeamCrest': awayTeamCrest1,
-					'GameWeek': str(gameWeek),
-					'Winner': winner,
-					'HomeScore': str(homeTeamScore),
-					'AwayScore': str(awayTeamScore),
+					'TeamName': TeamName,
+					'position': str(position),
+					'crestUrl': crestUrl,
+					'gamesPlayed': str(gamesPlayed),
+					'won': str(won),
+					'draw': str(draw),
+					'lost': str(lost),
+					'points': str(points),
+					'goalDifference': str(goalDifference),
 					'createdTime': str(datetime.today())
 				}
 			)
 		
-		if fixture["matchday"] == 26:
-			startTime = fixture["utcDate"].split("T")[1].split(":")
-			startTime2 = startTime[0]+":"+startTime[1]
-			startDate = fixture["utcDate"].split("T")[0]
-			# print(startTime2)
-			homeTeam = fixture["homeTeam"]["name"]
-			homeTeamCrest2 = crests[homeTeam]
-			awayTeam = fixture["awayTeam"]["name"]
-			awayTeamCrest2 = crests[awayTeam]
-			if homeTeam in abbreviations:
-				homeTeam = abbreviations[homeTeam]
-			if awayTeam in abbreviations:
-				awayTeam = abbreviations[awayTeam]
-			gameWeek = fixture["season"]["currentMatchday"]
+		dynamodb = boto3.resource('dynamodb')
+		tableName1 = 'PLFixturesDB-develop'
+		FixturesTable = dynamodb.Table(tableName1)
+		tableName2 = 'PLResultsDB-develop'
+		ResultsTable = dynamodb.Table(tableName2)
+		scan2 = FixturesTable.scan()
+		with FixturesTable.batch_writer() as batch:
+			for each in scan2['Items']:
+				batch.delete_item(Key={'FixtureID':each['FixtureID']})
+				
+		scan1 = ResultsTable.scan()
+		with ResultsTable.batch_writer() as batch:
+			for each in scan1['Items']:
+				batch.delete_item(Key={'MatchID':each['MatchID']})
+
+		for fixture in response2['matches']:
+			if fixture['matchday'] > fixture['season']['currentMatchday'] + 2:
+				break
+			# hard coded, change
+			if str(fixture['matchday']) == str(gamePeriod):
+				homeTeam = fixture['homeTeam']['name']
+				homeTeamCrest1 = crests[homeTeam]
+				awayTeam = fixture['awayTeam']['name']
+				awayTeamCrest1 = crests[awayTeam]
+				if homeTeam in abbreviations:
+					homeTeam = abbreviations[homeTeam]
+				if awayTeam in abbreviations:
+					awayTeam = abbreviations[awayTeam]
+
+				gameWeek = fixture['season']['currentMatchday']
+				homeTeamScore = fixture['score']['fullTime']['homeTeam']
+				awayTeamScore = fixture['score']['fullTime']['awayTeam']
+				if homeTeamScore == None and awayTeamScore == None:
+					homeTeamScore = '-'
+					awayTeamScore = '-'
+				if fixture['score']['winner'] == 'HOME_TEAM':
+					winner = fixture['homeTeam']['name']
+				elif fixture['score']['winner'] == 'AWAY_TEAM':
+					winner = fixture['awayTeam']['name']
+				else:
+					winner = 'Draw'
+
+				ResultsTable.put_item(
+					Item={
+						'MatchID': homeTeam + '-' + awayTeam,
+						'HomeTeam': homeTeam,
+						'HomeTeamCrest': homeTeamCrest1,
+						'AwayTeam': awayTeam,
+						'AwayTeamCrest': awayTeamCrest1,
+						'GameWeek': str(gameWeek),
+						'Winner': winner,
+						'HomeScore': str(homeTeamScore),
+						'AwayScore': str(awayTeamScore),
+						'createdTime': str(datetime.today())
+					}
+				)
+			# hard coded, change
+			if fixture['matchday'] == fixture['season']['currentMatchday']:
+				startTime = fixture['utcDate'].split('T')[1].split(':')
+				startTime2 = startTime[0]+':'+startTime[1]
+				startDate = fixture['utcDate'].split('T')[0]
+				# print(startTime2)
+				homeTeam = fixture['homeTeam']['name']
+				homeTeamCrest2 = crests[homeTeam]
+				awayTeam = fixture['awayTeam']['name']
+				awayTeamCrest2 = crests[awayTeam]
+				if homeTeam in abbreviations:
+					homeTeam = abbreviations[homeTeam]
+				if awayTeam in abbreviations:
+					awayTeam = abbreviations[awayTeam]
+				gameWeek = fixture['season']['currentMatchday']
 
 
-			FixturesTable.put_item(
-				Item={
-					'FixtureID': homeTeam + "-" + awayTeam,
-					'HomeTeam': homeTeam,
-					'HomeTeamCrest': homeTeamCrest2,
-					'AwayTeam': awayTeam,
-					'AwayTeamCrest': awayTeamCrest2,
-					'GameWeek': str(gameWeek),
-					'createdTime': str(datetime.today()),
-					'startTime': str(startTime2),
-					'startDate': str(startDate)
+				FixturesTable.put_item(
+					Item={
+						'FixtureID': homeTeam + '-' + awayTeam,
+						'HomeTeam': homeTeam,
+						'HomeTeamCrest': homeTeamCrest2,
+						'AwayTeam': awayTeam,
+						'AwayTeamCrest': awayTeamCrest2,
+						'GameWeek': str(gameWeek),
+						'createdTime': str(datetime.today()),
+						'startTime': str(startTime2),
+						'startDate': str(startDate)
+					}
+				)
+		
+		lambda_client = boto3.client('lambda')
+		lambda_client.invoke(
+			FunctionName = 'arn:aws:lambda:eu-west-1:706350010776:function:unlockLeagues-develop',
+		)
+		
+		schedulerTable.update_item(
+			Key={
+			'GamePeriod': 'Testing'
+			},
+			UpdateExpression='set GameWeek=:val1, Deadline=:val2',
+			ExpressionAttributeValues={
+				':val1': str(currWeek),
+				':val2': str(firstGameTime + " " + firstGameDate)
+			},
+			ReturnValues='UPDATED_NEW'
+		)
+
+		minutes = firstGameTime.split(':')[1]
+		hours = firstGameTime.split(':')[0]
+		dayOfMonth = firstGameDate.split('-')[2]
+		month = firstGameDate.split('-')[1]
+		year = firstGameDate.split('-')[0]
+		myCron = "cron({} {} {} {} ? {})".format(minutes,hours,dayOfMonth,month,year)
+
+		client = boto3.client('events')
+		rule = client.put_rule(
+			Name="MyRuleId",
+			ScheduleExpression=myCron,
+			State="ENABLED",
+		)
+		client.put_targets(
+			Rule="MyRuleId",
+			Targets=[
+				{
+					"Id": "MyTargetId",
+					"Arn": "arn:aws:lambda:eu-west-1:706350010776:function:lockLeagues-develop",
 				}
+			]
+		)
+		try:
+			lambda_client.add_permission(
+				FunctionName="arn:aws:lambda:eu-west-1:706350010776:function:lockLeagues-develop",
+				StatementId="MyRuleID",
+				Action="lambda:InvokeFunction",
+				Principal="events.amazonaws.com",
+				SourceArn=rule["RuleArn"]
 			)
+		except:
+			print('Error occured')
+
 
 	return {
 	'message': 'Hello from your new Amplify Python lambda!'
