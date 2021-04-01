c08b29c8cafa2fd854b08bf4bfd70a83b2935c06
tcal97 <tom.callaghan4@mail.dcu.ie>
2021-04-01 12:20:21 +0100

    created virtual env for CI/CD

--- src/lastmanstanding/tests/test_getLeagueInfo.py
+++ src/lastmanstanding/tests/test_getLeagueInfo.py
@@ -1,9 +1,7 @@
 import boto3
 import pytest
 from moto import mock_dynamodb2
-import sys
-sys.path.insert(1,'../amplify/backend/function/getLeagueInfo/src/')
-from index import *
+from amplify.backend.function.getLeagueInfo.src.index import queryDB
 
 # mock AWS services
 @mock_dynamodb2
